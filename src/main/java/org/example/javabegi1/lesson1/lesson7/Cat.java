f752ebd0e629a964bce4c69cedc4cb165888acc6
alexpuzi <alex.puzi@gmail.com>
2020-06-02 20:34:41 +0300

    ДЗ к 7 уроку

---
+++ src/main/java/org/example/javabegi1/lesson1/lesson7/Cat.java
@@ -0,0 +1,23 @@
+package org.example.javabegi1.lesson1.lesson7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+    public Cat(String name, int appetite, boolean satiety){
+        this.name = name;
+        this.appetite = appetite;
+    }
+    public boolean doSatiety(int n){
+        if(appetite<=n){
+            return true;
+        }
+        return false;
+    }
+    public void eat(Plate p){
+        p.decreaseFood(appetite, name);
+    }
+    public int doAppetite(){
+        return appetite;
+    }
+}
---
+++ src/main/java/org/example/javabegi1/lesson1/lesson7/MainForLesson7OnlyCat.java
@@ -0,0 +1,30 @@
+package org.example.javabegi1.lesson1.lesson7;
+
+public class MainForLesson7OnlyCat {
+    public static void main(String[] args) {
+        createСatsAndPlats();
+
+    }
+
+    public static int doRandomForEat() {
+        int i = (int) (Math.random() * 100);
+        return i;
+    }
+
+    public static void createСatsAndPlats() {
+        Cat[] cat = new Cat[5];
+        cat[0] = new Cat("Felix_0", doRandomForEat(), false);
+        cat[1] = new Cat("Felix_1", doRandomForEat(), false);
+        cat[2] = new Cat("Felix_2", doRandomForEat(), false);
+        cat[3] = new Cat("Felix_3", doRandomForEat(), false);
+        cat[4] = new Cat("Felix_4", doRandomForEat(), false);
+        Plate plate = new Plate(doRandomForEat());
+        for (int i = 0; i < cat.length; i++) {
+            System.out.println("/************************\'");
+            plate.info();
+            cat[i].eat(plate);
+            plate.addEatInPlate(cat[i].doAppetite());
+            System.out.println(cat[i].doSatiety(plate.returnFood()));
+        }
+    }
+}
