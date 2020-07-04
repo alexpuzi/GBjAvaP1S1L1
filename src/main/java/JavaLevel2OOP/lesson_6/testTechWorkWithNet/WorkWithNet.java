ed5de3848353c74f265c71afdcb979b8090be7ce
alexpuzi <alex.puzi@gmail.com>
2020-07-04 07:18:52 +0300

    check in. Сдам ДЗ 5.06.2020

---
+++ src/main/java/JavaLevel2OOP/lesson_6/testTechWorkWithNet/WorkWithNet.java
@@ -0,0 +1,10 @@
+package JavaLevel2OOP.lesson_6.testTechWorkWithNet;
+
+import java.net.Socket;
+
+public class WorkWithNet {
+    public static void main(String[] args) {
+        Socket socket = null;
+
+    }
+}
