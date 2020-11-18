0aa460aa0353978a492854f1b66319ca67947ab9
alexpuzi <alex.puzi@gmail.com>
2020-11-18 22:17:30 +0300

    к уроку 5. в DZ5Lesson5Main

--- src/main/java/JavaLevel3OOP/lesson_4/Car.java
+++ src/main/java/JavaLevel3OOP/lesson_4/Car.java
@@ -1,5 +1,8 @@
 package JavaLevel3OOP.lesson_4;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Car implements Runnable {
     public static int CARS_COUNT;
     static {
@@ -8,17 +11,20 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private CyclicBarrier barrier;
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
     public String getName(){
         return name;
     }
     public int getSpeed(){
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier barrier) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.barrier = barrier;
     }
     @Override
     public void run() {
@@ -26,11 +32,18 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            barrier.await();
+            // пока печатается System.out.println(this.name + " готовится"); все попадают к линии старта
+            barrier.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            if(atomicInteger.incrementAndGet() == 1) {
+                System.out.println(name + " " + "- WIN!");
+            }
+            barrier.await();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
--- src/main/java/JavaLevel3OOP/lesson_4/DZ5Lesson5Main.java
+++ src/main/java/JavaLevel3OOP/lesson_4/DZ5Lesson5Main.java
@@ -1,19 +1,29 @@
 package JavaLevel3OOP.lesson_4;
 
+import java.util.concurrent.CyclicBarrier;
+
 public class DZ5Lesson5Main {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ ПОДГОТОВКА >>> ПОДГОТОВКА!!!");
+//        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ ПОДГОТОВКА >>> ПОДГОТОВКА!!!");
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT);
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), barrier);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            barrier.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            barrier.await();
+            barrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
